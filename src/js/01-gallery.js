afe858b16eef4f109113a0a1b271f6ef484c57bc
Ront31 <kolya.maslov.09@gmail.com>
2023-03-09 15:52:51 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,5 @@
-import { galleryItems } from './gallery-items.js';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const divGalleryEl = document.querySelector('.gallery');
-const galleryMurkup = createGallery(galleryItems);
-divGalleryEl.insertAdjacentHTML('beforeend', galleryMurkup);
-
-// divGalleryEl.addEventListener("click", onGalleryImgClick);
-
-function createGallery(arg) {
-  return arg
-    .map(({ preview, original, description }) => {
-      return `
-          <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
-        `;
-    })
-    .join('');
-}
-
-new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-  captionsData: 'alt',
-});
